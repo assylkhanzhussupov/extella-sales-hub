7c29347cbe20fe2e24167e17b479f4db64d8feb9
assylkhanzhussupov <assylkhan.zhussupov@gmail.com>
2026-06-09 17:51:48 +0500

    feat: add supabase_service_key and admin_email to config

--- app/config.py
+++ app/config.py
@@ -5,9 +5,11 @@ class Settings(BaseSettings):
     telegram_bot_token: str = ""
     b24_webhook: str = ""
     supabase_url: str = ""
-    supabase_key: str = ""
+    supabase_key: str = ""          # anon key
+    supabase_service_key: str = ""  # service_role key
     app_env: str = "development"
     secret_key: str = ""
+    admin_email: str = "assylkhan.zhussupov@gmail.com"
 
     class Config:
         env_file = ".env"
